0d1479be810c97496258b31b8af67f1a6cffe99b
tsubokura-shuhei <repeat2639@gmail.com>
2024-10-18 22:26:37 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "",
+  output: 'export',
+  trailingSlash: true,
+};
 
 export default nextConfig;
