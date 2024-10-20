737ec271388ef83cba7f19fa6178ed3445797f71
tsubokura-shuhei <repeat2639@gmail.com>
2024-10-20 22:50:19 +0900

    file fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //(公開用パス) basePath: "/test2", //パスを指定（例：https://limeokapi95.sakura.ne.jp/test2/）
+  // basePath: "/test2", // (公開用のパスを指定)（例：https://limeokapi95.sakura.ne.jp/test2/）
   basePath: "", //（ローカル用パス）
   output: "export",
   trailingSlash: true,
